378695c135777761df0965a00fb6bab9511ed6f1
Caio Emmanuel <caioesr@ibm.com>
2023-08-28 11:01:11 -0300

    adicionando link testpypi

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cuddly Octo Potato
 
 Pacote para ver o último commit feito no [repositório da matéria](https://github.com/Insper/open-dev).
+
+Pacote disponível [aqui](https://test.pypi.org/project/dev-aberto-caioesr/).
--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='dev_aberto_caioesr',
-      version='0.0.1',
-      author='Caio Emmanuel',
-      author_email='caioemmanuelsr@gmail.com',
-      url='https://github.com/cemmanuelsr/cuddly-octo-potato',
-      python_requires='>=3.9',
-      platforms=['linux_x86_64'],
-      install_requires=['requests'],
-      scripts=['scripts/hello.py'],
-      packages=['dev_aberto']
-      )
+setup(
+    name='dev_aberto_caioesr',
+    version='0.1',
+    packages=['dev_aberto'],
+    author='Caio Emmanuel',
+    author_email='caioemmanuelsr@gmail.com',
+    install_requires=['requests'],
+    scripts=['scripts/hello.py']
+)
